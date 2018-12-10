ec1fad1a2f39dce87cf2ead24f2ced13bb9da35a
jipeng <jipeng@able-elec.com>
2018-12-10 15:24:00 +0800

    添加监听:pathChildrenCache

--- zkstudy1/src/test/java/com/able/zkstudy/ApacheCuraotrStudy.java
+++ zkstudy1/src/test/java/com/able/zkstudy/ApacheCuraotrStudy.java
@@ -9,9 +9,7 @@ import org.apache.curator.framework.api.BackgroundCallback;
 import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.framework.api.CuratorWatcher;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
-import org.apache.curator.framework.recipes.cache.ChildData;
-import org.apache.curator.framework.recipes.cache.NodeCache;
-import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.cache.*;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.retry.RetryNTimes;
 import org.apache.curator.retry.RetryUntilElapsed;
@@ -25,6 +23,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -158,69 +157,118 @@ public class ApacheCuraotrStudy {
         try {
             //读取节点的数据
             String path = "/super/imooc0000000002";
-            Stat stat=new Stat();
+            Stat stat = new Stat();
             byte[] bytes = curator.getData()
                     .storingStatIn(stat)
                     .forPath(path);
             log.info("读取到的数据为:{}", new String(bytes));
-            log.info("version={}",stat.getVersion());
+            log.info("version={}", stat.getVersion());
 
 
             List<String> list = curator.getChildren().forPath("/");
-            log.info("查询到子节点的数据为:{}",list);
+            log.info("查询到子节点的数据为:{}", list);
 
             Stat stat1 = curator.checkExists().forPath("/man1");
-            log.info("节点是否存在：{}",stat1!=null?"true":"false");
+            log.info("节点是否存在：{}", stat1 != null ? "true" : "false");
 
         } finally {
             curator.close();
         }
     }
+
     @Test
-    public void testWatch () throws Exception{
-        String path="/man";
+    public void testWatch() throws Exception {
+        String path = "/man";
         //使用usingWatcher的时候只会触发一次监听 监听玩就销毁
         curator.getData().usingWatcher(new CuratorWatcher() {
             @Override
             public void process(WatchedEvent event) throws Exception {
-                log.info("event={}",event);
+                log.info("event={}", event);
                 log.info(event.getPath());
             }
         }).forPath(path);
         TimeUnit.SECONDS.sleep(100);
     }
+
     @Test
-    public void testWatch1 () throws Exception{
-        CountDownLatch countDownLatch=new CountDownLatch(1);
+    public void testWatch1() throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
         //为节点添加watcher
         //NodeCache:监听数据节点的变更 会触发监听事件
-        String path="/man";
-        NodeCache nodeCache=new NodeCache(curator,path);
+        String path = "/man";
+        NodeCache nodeCache = new NodeCache(curator, path);
         //buildInitial:初始化的时候获取node的值 并且缓存
         nodeCache.start(true);
         ChildData currentData = nodeCache.getCurrentData();
         if (currentData != null) {
-            log.info("节点初始化的数据为:{}",new String(currentData.getData()));
-        }else {
+            log.info("节点初始化的数据为:{}", new String(currentData.getData()));
+        } else {
             log.info("节点初始化的数据为空");
         }
         nodeCache.getListenable().addListener(new NodeCacheListener() {
             @Override
             public void nodeChanged() throws Exception {
                 ChildData currentData1 = nodeCache.getCurrentData();
-                if (currentData1!=null) {
-                    log.info("节点路径:{},节点数据:{}",currentData1.getPath(),new String(currentData1.getData()));
-                }else {
+                if (currentData1 != null) {
+                    log.info("节点路径:{},节点数据:{}", currentData1.getPath(), new String(currentData1.getData()));
+                } else {
                     log.info("节点已经被删除");
                 }
-                if (currentData==null&&currentData1!=null) {
-                    log.info("节点创建:{},节点数据:{}",currentData1.getPath(),new String(currentData1.getData()));
+                if (currentData == null && currentData1 != null) {
+                    log.info("节点创建:{},节点数据:{}", currentData1.getPath(), new String(currentData1.getData()));
                 }
-                   countDownLatch.countDown();
+                countDownLatch.countDown();
             }
         });
         countDownLatch.await();
-
     }
+
+    @Test
+    public void testWatch2() throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        String path = "/";
+        PathChildrenCache pathChildrenCache = new PathChildrenCache(curator, path, true);
+        /**
+         * startModel：初始化方式
+         *      POST_INITIALIZED_EVENT: 异步初始化 初始化后会触发事件
+         *      NORMAL:异步初始化
+         *      BUILD_INITIAL_CACHE:同步初始化
+         */
+        //pathChildrenCache.start(PathChildrenCache.StartMode.BUILD_INITIAL_CACHE);
+        //pathChildrenCache.start(PathChildrenCache.StartMode.BUILD_INITIAL_CACHE);
+        pathChildrenCache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+        List<ChildData> currentData = pathChildrenCache.getCurrentData();
+        if (currentData != null) {
+            currentData.forEach(x -> log.info("子节点路径为:{}数据为:{}", x.getPath(), new String(x.getData())));
+        }
+        //添加监听事件
+        pathChildrenCache.getListenable().addListener(new PathChildrenCacheListener() {
+            @Override
+            public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
+                //初始化
+                if (PathChildrenCacheEvent.Type.INITIALIZED.equals(event.getType())) {
+                    log.info("子节点初始化成功");
+                } else if (PathChildrenCacheEvent.Type.CHILD_ADDED.equals(event.getType())) {
+                    ChildData data = event.getData();
+                    log.info("子节点添加:path={},data={}", data.getPath(), new String(data.getData()));
+                } else if (PathChildrenCacheEvent.Type.CHILD_REMOVED.equals(event.getType())) {
+                    log.info("删除子节点");
+                    ChildData data = event.getData();
+                    log.info("path={},data={}", data.getPath(), new String(data.getData()));
+
+                } else if (PathChildrenCacheEvent.Type.CHILD_UPDATED.equals(event.getType())) {
+                    log.info("子节点更新");
+                    ChildData data = event.getData();
+                    log.info("path={},data={}", data.getPath(), new String(data.getData()));
+
+                }
+
+
+            }
+        });
+
+        countDownLatch.await();
+    }
+
 }
 
