ffd3c43ca2969323af7240d2bcc8532e97d2c9f3
jipeng <jipeng@able-elec.com>
2019-01-24 10:44:25 +0800

    elk整合springboot CRUD代码编写

--- elk/esstudy/src/main/java/com/able/esstudy/controller/BookController.java
+++ elk/esstudy/src/main/java/com/able/esstudy/controller/BookController.java
@@ -2,14 +2,30 @@ package com.able.esstudy.controller;
 
 import com.able.esstudy.model.Book;
 import com.able.esstudy.utils.JsonUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author jipeng
@@ -19,23 +35,79 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("book")
 public class BookController {
+    public static final String INDEX_OF_BOOK="book";
+    public static final String TYPE_OF_BOOK="novel";
     @Resource
     TransportClient client;
-    @GetMapping("{id}")
+    @RequestMapping("/query/{id}")
     public ResponseEntity queryById(@PathVariable("id") String id){
 
-        GetResponse response = client.prepareGet("book", "novel", id).get();
+        GetResponse response = client.prepareGet(INDEX_OF_BOOK,TYPE_OF_BOOK, id).get();
         if (!response.isExists()) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity(response.getSource(), HttpStatus.OK);
     }
-    @PostMapping("save")
+    @PutMapping("save")
     public ResponseEntity save(Book book){
         String jsonString = JsonUtil.obj2String(book);
-        IndexResponse response = client.prepareIndex("book", "novel", "13").setSource(jsonString).get();
+        IndexResponse response = client.prepareIndex(INDEX_OF_BOOK,TYPE_OF_BOOK, "15").setSource(jsonString, XContentType.JSON).get();
 
         return ResponseEntity.ok(response.getId());
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity deleteById(@PathVariable("id") String id){
+        DeleteResponse response = client.prepareDelete(INDEX_OF_BOOK, TYPE_OF_BOOK, id).get();
+
+        return new ResponseEntity(response.status(),HttpStatus.OK);
+    }
+
+    @PostMapping("update/{id}")
+    public ResponseEntity update(@PathVariable("id") String id,@RequestParam(required = false,defaultValue = "") String author
+            ,@RequestParam(required = false,defaultValue = "")String title) throws Exception{
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.index(INDEX_OF_BOOK);
+        updateRequest.type(TYPE_OF_BOOK);
+        updateRequest.id(id);
+
+        XContentBuilder xContentBuilder = XContentFactory.jsonBuilder().startObject();
+        if (!StringUtils.isEmpty(title)) {
+            xContentBuilder.field("title",title);
+        }
+        if (!StringUtils.isEmpty(author)) {
+            xContentBuilder.field("author",author);
+        }
+        xContentBuilder.endObject();
+        updateRequest.doc(xContentBuilder);
+        UpdateResponse response = client.update(updateRequest).get();
+        return ResponseEntity.ok(response.getResult().toString());
+    }
+
+    @GetMapping("query")
+    public ResponseEntity queryByCond(@RequestParam(name = "author",required = false,defaultValue = "") String author,
+                                      @RequestParam(name = "title",required = false,defaultValue = "") String title,
+                                      @RequestParam(name = "gtWorlds", required = false) Integer gtWorlds,
+                                      @RequestParam(name = "ltWorlds",required = false) Integer ltWorlds
+                                      ){
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        if (!StringUtils.isEmpty(author)) {
+            boolQueryBuilder.must(QueryBuilders.termQuery("author",author));
+        }
+        if (!StringUtils.isEmpty(title)) {
+            boolQueryBuilder.must(QueryBuilders.matchQuery("title",title));
+        }
+        SearchRequestBuilder searchRequestBuilder = client.prepareSearch().setIndices(INDEX_OF_BOOK).setTypes(TYPE_OF_BOOK)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(boolQueryBuilder)
+                .setFrom(0)
+                .setSize(10);
+        SearchResponse searchResponse = searchRequestBuilder.get();
+        List<Map<String,Object>> list=new ArrayList<>();
+        searchResponse.getHits().forEach(x->list.add(x.getSource()));
+        return ResponseEntity.ok(list);
+
+
     }
 
 }
--- elk/esstudy/src/test/java/com/able/esstudy/controller/BookControllerTest.java
+++ elk/esstudy/src/test/java/com/able/esstudy/controller/BookControllerTest.java
@@ -35,23 +35,54 @@ public class BookControllerTest {
     }
     @Test
     public void testQueryById() throws Exception{
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/book/1").accept(MediaType.APPLICATION_JSON_UTF8_VALUE)).andReturn();
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/book/query/1").accept(MediaType.APPLICATION_JSON_UTF8_VALUE)).andReturn();
         String contentAsString = mvcResult.getResponse().getContentAsString();
         System.out.println(mvcResult);
         log.info("查询到的结果数据为:{}",contentAsString);
     }
     @Test
     public void testSave () throws Exception{
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/book/save")
-                .param("title", "火影忍者")
-                .param("author", "岸本齐史")
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.put("/book/save")
+                .param("title", "灌篮高手")
+                .param("author", "井上雄彥")
                 .param("world_count", "20000")
-                .param("publish_date", "2019-01-23").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .param("publish_date", "2011-01-23").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andReturn();
         MockHttpServletResponse response = mvcResult.getResponse();
         log.info("响应的状态码为:{}",response.getStatus());
         String contentAsString = response.getContentAsString();
         log.info("保存的内容id为:{}",contentAsString);
     }
+    @Test
+    public void testDelete () throws Exception{
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/book/delete/17").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        log.info("响应的状态码为:{}",response.getStatus());
+        String contentAsString = response.getContentAsString();
+        log.info("删除的内容响应状态为:{}",contentAsString);
+    }
+    @Test
+    public void testUpdate() throws Exception{
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .post("/book/update/1")
+                .param("title", "自来也的豪杰物语")
+                .param("author", "自来也").accept(MediaType.APPLICATION_JSON_UTF8_VALUE)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String contentAsString = response.getContentAsString();
+        log.info("更新的内容响应状态为:{}",contentAsString);
+    }
+    @Test
+    public void testQueryByCond()  throws Exception{
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/book/query")
+                .param("author", "自来也")
+                .param("title","灌篮高手")
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String contentAsString = response.getContentAsString();
+        log.info("查询到的数据为:{}",contentAsString);
+
+    }
+
 }
 
