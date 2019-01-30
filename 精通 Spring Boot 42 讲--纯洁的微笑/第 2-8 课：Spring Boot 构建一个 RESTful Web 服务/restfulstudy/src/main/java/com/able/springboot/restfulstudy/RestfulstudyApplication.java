b548b489bcdaa5be3b164c42a3a4a468b5f8b040
jipeng <jipeng@able-elec.com>
2019-01-30 09:08:48 +0800

    spring boot restful学习笔记

---
+++ 精通 Spring Boot 42 讲--纯洁的微笑/第 2-8 课：Spring Boot 构建一个 RESTful Web 服务/restfulstudy/src/main/java/com/able/springboot/restfulstudy/RestfulstudyApplication.java
@@ -0,0 +1,14 @@
+package com.able.springboot.restfulstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulstudyApplication.class, args);
+    }
+
+}
+
